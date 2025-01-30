3a43ebcc17322999ee40caf54a69e6606e0428ef
Ramon <ramon.navm@gmail.com>
2025-01-30 15:13:18 +0100

    first commit

---
+++ Scripts/Card.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+var position_in_hand
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_parent().connect_card_signals(self)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_mouse_entered():
+	emit_signal("hovered", self)
+
+
+func _on_area_2d_mouse_exited():
+	emit_signal("hovered_off", self)
