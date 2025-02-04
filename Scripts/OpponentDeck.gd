2c13177613b8ec76c2bd5e0f3426827fd833f39f
RCorpy <ramon.navm@gmail.com>
2025-02-04 01:02:12 +0100

    fixed the way the cards came from opponent

--- Scripts/OpponentDeck.gd
+++ Scripts/OpponentDeck.gd
@@ -4,14 +4,14 @@ const CARD_SCENE_PATH = "res://Scenes/Card.tscn"
 const CARD_DRAW_SPEED = 0.4
 const STARTING_HAND_SIZE = 5
 
-var player_deck = ["TowerYellow", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple"]
+var opponent_deck = ["TowerYellow", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple", "TowerBlue", "TowerPurple"]
 var card_database_reference
 var card_draw_this_turn = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player_deck.shuffle()
-	$RichTextLabel.text = str(player_deck.size())
+	opponent_deck.shuffle()
+	$RichTextLabel.text = str(opponent_deck.size())
 	card_database_reference = preload("res://Scripts/CardDataBase.gd")
 	for i in range(STARTING_HAND_SIZE):
 		draw_card()
@@ -24,15 +24,15 @@ func draw_card():
 		return
 		
 	card_draw_this_turn = true
-	var card_drawn_name = player_deck[0]
-	player_deck.erase(card_drawn_name)
+	var card_drawn_name = opponent_deck[0]
+	opponent_deck.erase(card_drawn_name)
 	
-	if player_deck.size() == 0:
+	if opponent_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
 		$Sprite2D.visible = false
 		$RichTextLabel.visible = false
 		
-	$RichTextLabel.text = str(player_deck.size())
+	$RichTextLabel.text = str(opponent_deck.size())
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
 	var card_img_path = str("res://Assets/" + str(card_drawn_name) + "Card.png")
@@ -40,7 +40,8 @@ func draw_card():
 	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
 	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
 	new_card.card_type = card_database_reference.CARDS[card_drawn_name][2]
+	new_card.position = Vector2(1780,125)
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
-	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
-	new_card.get_node("AnimationPlayer").play("card_flip")
+	$"../OpponentHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+	#new_card.get_node("AnimationPlayer").play("card_flip")
